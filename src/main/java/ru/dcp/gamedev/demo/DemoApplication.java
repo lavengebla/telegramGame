5cdc97e3760735d00137707955edf7a4e828ae23
Sergey <konditerov.s.a@google.com>
2021-03-12 15:40:47 +0300

    proccessorExecutor add

--- src/main/java/ru/dcp/gamedev/demo/DemoApplication.java
+++ src/main/java/ru/dcp/gamedev/demo/DemoApplication.java
@@ -1,10 +1,34 @@
 package ru.dcp.gamedev.demo;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@EnableAsync
+@Slf4j
+@EnableScheduling
 public class DemoApplication {
+
+	@Bean(name="processExecutor")
+	public TaskExecutor workExecutor() {
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setThreadNamePrefix("Async-");
+		threadPoolTaskExecutor.setCorePoolSize(3);
+		threadPoolTaskExecutor.setMaxPoolSize(3);
+		threadPoolTaskExecutor.setQueueCapacity(600);
+		threadPoolTaskExecutor.afterPropertiesSet();
+		log.info("ThreadPoolTaskExecutor set");
+		return threadPoolTaskExecutor;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
